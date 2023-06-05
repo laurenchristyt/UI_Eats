c1e367d8d14706f48982380d0d77cd2473bf284c
laurenchristyt <lauren.christy@ui.ac.id>
2023-06-05 22:05:33 +0700

    Backend Account Management

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const { testDatabaseConnection } = require('./src/config/config');
+const usersRoutes = require('./src/routes/userRoutes');
+
+app.get('/', (req, res) => {
+  res.send('Connection established');
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use('/users', usersRoutes);
+
+testDatabaseConnection();
+// Start the server
+const PORT = process.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
